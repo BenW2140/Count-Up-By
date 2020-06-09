aa6c6e64aadfb70923e318ff637bd9987a1c19fb
Ian Scott <chesnekov@gmail.com>
2020-06-09 10:31:35 -0700

    add logic to ensure user inputs appropriate/acceptable data
    Co-authored-by: Ben White <bwhite2140@outlook.com>

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,14 @@ $("document").ready(function () {
     const countTo = parseInt($("input#count-to").val());
     const countBy = parseInt($("input#count-by").val());
 
-    
-    for (let i = 0; i <= countTo; i += countBy) {
-      $("ul#output").append("<li>" + i + "</li>")
+    if (countTo <= 0 || countTo === NaN) {
+      alert("Please enter a positive number!");
+    } else if (countBy >= countTo) {
+      alert("We can't count to a number by a larger number than we are counting to!");
+    } else {
+      for (let i = 0; i <= countTo; i += countBy) {
+        $("ul#output").append("<li>" + i + "</li>")
+      }
     }
-
   });
 });
